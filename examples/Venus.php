8644f849c2cb6ae33f75bb1bd12d41bbf87e8684
Andreas Möller <moeller@doctari.de>
2018-09-19 16:51:58 +0200

    - Example for venus

--- examples/Venus.php
+++ examples/Venus.php
@@ -2,6 +2,7 @@
 
 include __DIR__ . '/../vendor/autoload.php';
 
+use Andrmoel\AstronomyBundle\AstronomicalObjects\Earth;
 use Andrmoel\AstronomyBundle\AstronomicalObjects\Planets\Venus;
 use Andrmoel\AstronomyBundle\Location;
 use Andrmoel\AstronomyBundle\TimeOfInterest;
@@ -13,31 +14,53 @@ $lon = AngleUtil::angle2dec('13°24\'37.91"');
 $location = new Location($lat, $lon);
 
 // Time of interest
-$dateTime = new DateTime('2018-01-10 05:00:00');
+$dateTime = new DateTime('2018-05-13 20:00:00');
 $toi = new TimeOfInterest($dateTime);
 
-// Create moon
-$moon = new Venus($toi);
+$earth = new Earth($toi);
+$e = $earth->getObliquityOfEcliptic();
 
-// Get moon's position in sky
-$localHorizontalCoordinates = $moon->getLocalHorizontalCoordinates($location);
-$azimuth = $localHorizontalCoordinates->getAzimuth();
+// Create venus
+$venus = new Venus($toi);
+
+// Get ecliptical coordinates
+$eclipticalCoordinates = $venus
+    ->getHeliocentricEclipticalCoordinates()
+    ->getEclipticalCoordinates($toi);
+
+$eclLatitude = $eclipticalCoordinates->getLatitude();
+$eclLatitude = AngleUtil::dec2angle($eclLatitude);
+$eclLongitude = $eclipticalCoordinates->getLongitude();
+$eclLongitude = AngleUtil::dec2angle($eclLongitude);
+
+// Get equatorial coordinates
+$equatorialCorodinates = $eclipticalCoordinates
+    ->getEquatorialCoordinates($e);
+
+$rightAscension = $equatorialCorodinates->getRightAscension();
+$rightAscension = AngleUtil::dec2time($rightAscension);
+$declination = $equatorialCorodinates->getDeclination();
+$declination = AngleUtil::dec2angle($declination);
+
+// Get local horizontal coordinates
+$localHorizontalCoordinates = $equatorialCorodinates->getLocalHorizontalCoordinates($location, $toi);
+$azimuth = $localHorizontalCoordinates->getAzimuth(); // TODO FALSCHER WERT. Laut Stellarium 294.45...
 $azimuth = AngleUtil::dec2angle($azimuth);
 $altitude = $localHorizontalCoordinates->getAltitude();
 $altitude = AngleUtil::dec2angle($altitude);
-$isWaxingMoon = $moon->isWaxingMoon() ? 'yes' : 'no';
-$illuminatedFraction = $moon->getIlluminatedFraction();
-$positionAngleOfBrightLimb = $moon->getPositionAngleOfMoonsBrightLimb();
 
 echo <<<END
 +------------------------------------
 | Moon
 +------------------------------------
 Date: {$toi->getDateTime()->format('Y-m-d H:i:s')}
+Ecliptical latitude: {$eclLatitude}
+Ecliptical longitude: {$eclLongitude}
+Right ascending: {$rightAscension}
+Declination: {$declination}
+
+Position: {$lat}°, {$lon}°
 Azimuth: {$azimuth}
 Altitude: {$altitude}
-Is waxing moon: {$isWaxingMoon}
-Illuminated fraction: {$illuminatedFraction}
-Position angle of bright limb: {$positionAngleOfBrightLimb}
 
 END;
