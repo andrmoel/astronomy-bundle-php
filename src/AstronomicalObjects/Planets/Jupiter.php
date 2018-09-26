b685b87934b0cb84722a3bdba6a3086b9a527fa8
Andreas Möller <moeller@doctari.de>
2018-09-26 16:31:32 +0200

    - Removed TODO

--- src/AstronomicalObjects/Planets/Jupiter.php
+++ src/AstronomicalObjects/Planets/Jupiter.php
@@ -6,7 +6,6 @@ class Jupiter extends Planet
 {
     public function loadVSOP87Data(): array
     {
-        // TODO Ecliptical longitude falsch
         $data = file_get_contents(self::VSOP87_FILE_PATH . 'jupiter.json');
 
         return json_decode($data, 1);
