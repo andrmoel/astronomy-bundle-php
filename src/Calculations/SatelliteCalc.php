4512ec03f060c7b92074daf2e8e40c9da2fd814a
Andreas Möller <andrmoel@gmail.com>
2019-06-04 23:31:29 +0200

    - Satellite calculations

--- src/Calculations/SatelliteCalc.php
+++ src/Calculations/SatelliteCalc.php
@@ -2,7 +2,10 @@
 
 namespace Andrmoel\AstronomyBundle\Calculations;
 
+use Andrmoel\AstronomyBundle\Constants;
 use Andrmoel\AstronomyBundle\Entities\TwoLineElements;
+use Andrmoel\AstronomyBundle\Utils\AngleUtil;
+use Andrmoel\AstronomyBundle\Utils\GeneralUtil;
 
 class SatelliteCalc
 {
@@ -18,7 +21,8 @@ class SatelliteCalc
 
         $M = self::getMeanAnomaly($dT, $M0, $n, $d1mm, $d2mm);
 
-        var_dump($M);die();
+        var_dump($M);
+        die();
         // TODO ...
         return [];
     }
@@ -31,5 +35,51 @@ class SatelliteCalc
             + $d2mm * pow($dT, 3);
 
         return $M;
+    }
+
+    private static function getTrueAnomaly(float $E, float $e): float
+    {
+        $ERad = deg2rad($E);
+
+        $ThetaRad = 2 * atan(
+                sqrt((1 + $e) / (1 - $e)) * tan($ERad / 2)
+            );
+
+        $Theta = rad2deg($ThetaRad);
+
+        return $Theta;
+    }
+
+    private static function getEccentricAnomaly(float $M, float $e): float
+    {
+        $MRad = deg2rad($M);
+
+        $Ei = $M + 0.85 * $e * GeneralUtil::sign(sin($MRad));
+
+        do {
+            $EiRad = deg2rad($Ei);
+
+            $Ej = $Ei - ($Ei - $e * sin($EiRad) - $M) / (1 - $e * cos($EiRad));
+            $Ei = $Ej;
+        } while (abs($Ei - $Ej) > 1e-8);
+
+        $Ei = AngleUtil::normalizeAngle($Ei);
+
+        return $Ei;
+    }
+
+    private static function getSemiMajorAxis(float $dT, float $n, float $d1mm, float $d2mm): float
+    {
+        $GM = Constants::GRAVITATIONAL_PARAMETER;
+
+        $a0 = pow(sqrt($GM / $n), 2 / 3);
+
+        $term = $n
+            + 2 * $d1mm * $dT
+            + 3 * $d2mm * pow($dT, 2);
+
+        $a = $a0 * pow($n * pow($term, -1), 2 / 3);
+
+        return $a;
     }
 }
